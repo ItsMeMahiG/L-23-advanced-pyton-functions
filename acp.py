f5fcb5a3a06b64dcd64a042ec63da22ce28e200e
ItsMeMahiG <mahi.gadicherlaboba@gmail.com>
2024-12-30 10:11:18 +0530

    hi

--- acp.py
+++ acp.py
@@ -1,4 +1,4 @@
-num= int(input("under which value do you want your odd and even numbers? : "))
+num= int(input("Under which value do you want your odd and even numbers? : "))
 ol=[i for i in range (num)if i%2!=0]
 print("list of odd numbers:", ol,"\n")
 el=[i for i in range (num)if i%2==0]
